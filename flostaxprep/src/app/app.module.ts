9c1bc99f205101131d5435afef23bd4cea87895a
MarkAnthony <20306343+MuscleGuy@users.noreply.github.com>
2018-07-04 23:07:24 -0400

    first merge

---
+++ flostaxprep/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SolutionsComponent } from './components/solutions/solutions.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SolutionsComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
